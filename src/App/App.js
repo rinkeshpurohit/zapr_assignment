d37cf6c55437586018667e826f6bcb79725ae247
rinkeshpurohit <rinkeshpurohit@gmail.com>
2018-01-12 04:10:57 +0530

    pagination

--- src/App/App.js
+++ src/App/App.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 import './App.css'
 
 import ProductListingComponent from '../ProductListingComponent/ProductListingComponent';
-import TreeNode from "../FiltersComponent/TreeNode";   
+import TreeNode from "../FiltersComponent/TreeNode";
+import Paginator from "../Paginator/Paginator";
 
 class App extends Component {
     constructor() {
@@ -16,10 +17,12 @@ class App extends Component {
             'categories': [],
             'filters': [],
             'displayedPdts': [],
+            'slicedPdts': [],
             'sortBy': _this.sort[0]
         };
 
         this.handleFilterSelection = this.handleFilterSelection.bind(this);
+        this.handleSlicedArray = this.handleSlicedArray.bind(this);
         this.sortProducts = this.sortProducts.bind(this);
     }
 
@@ -33,6 +36,7 @@ class App extends Component {
     }
 
     filterProducts(filters) {
+        let _this = this;
         let filteredProducts = [];
         let products = this.state.products;
 
@@ -49,6 +53,8 @@ class App extends Component {
         console.log('filtered', filteredProducts.length);
         this.setState({
             'displayedPdts' : filteredProducts
+        },()=> {
+            _this.sortDisplayedPdts(this.state.sortBy);
         });
     }
 
@@ -83,6 +89,7 @@ class App extends Component {
     }
 
     sortDisplayedPdts(key) {
+        let _this = this;
         let products = this.state.displayedPdts.slice(0);
         let sorted = products.sort((a,b) => {
             return b[key] - a[key];
@@ -90,14 +97,28 @@ class App extends Component {
 
         this.setState({
             displayedPdts: sorted
+        },()=> {
+            _this.handleSlicedArray([0, 10]);
         })
     }
 
     clearAllFilters() {
+        let _this = this;
         let products = this.state.products;
         this.setState({
             filters: [],
             displayedPdts: products 
+        },()=>{
+            _this.sortDisplayedPdts(this.state.sortBy);
+        });
+    }
+
+    handleSlicedArray(sliceIndesxes) {
+        let products = this.state.displayedPdts;
+        let sliced = Array.prototype.slice.apply(products,sliceIndesxes);
+
+        this.setState({
+            'slicedPdts': sliced
         });
     }
 
@@ -105,6 +126,8 @@ class App extends Component {
         let tree1 = this.state.categories.map(function (child) {
             return <TreeNode key={child.id} data={child} onFilterSelection={this.handleFilterSelection} />;
         }.bind(this));
+
+        let total = this.state.displayedPdts.length;
 
         return (
             <section className="container-fluid">
@@ -122,7 +145,7 @@ class App extends Component {
                     <div className="products-header row">
                         <div className="col-md-6 category-meta">
                             <h3 className="category-title">Category Name</h3>
-                            <p className="product-count">Showing {this.state.displayedPdts.length} products</p>
+                            <p className="product-count">Showing {total} products</p>
                         </div>
                         <div className="col-md-6 sort-section">
                             <select className="pull-right" name="Sort by" 
@@ -135,7 +158,16 @@ class App extends Component {
                             </select>
                         </div>
                     </div>
-                    <ProductListingComponent products={this.state.displayedPdts} />
+                    {
+                        (total===0)
+                        ? <p> Loading......</p>
+                        : (
+                            <div>
+                                <ProductListingComponent products={this.state.slicedPdts} />
+                                <Paginator sliceArray={this.handleSlicedArray} totalItems={total} />
+                            </div>
+                        )
+                    }
                 </div>
             </section>
         )
---
+++ src/Paginator/Paginator.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+
+export default class Paginator extends Component {
+    constructor(props) {
+        super(props);
+
+        const perPage = 10;
+        let totalItems = this.props.totalItems;
+        let totalPage = Math.ceil(totalItems / perPage);
+
+        this.state = {
+            'initialPage': 1,
+            'perPage': perPage,
+            'currPage': 1,
+            'totalPage': totalPage
+        };
+
+        this.handlePageChange = this.handlePageChange.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const perPage = 10;
+        let totalItems = nextProps.totalItems;
+        let totalPage = Math.ceil(totalItems / perPage);
+
+        this.setState({
+            'currPage': 1,
+            'totalPage': totalPage
+        });       
+    }
+
+
+    handlePageChange(direction) {
+        let currPage = this.state.currPage;
+        let totalPage = this.state.totalPage;
+        let perPage = this.state.perPage;
+        let sliceIndexes = [];
+
+        (direction>0) ? currPage++ : currPage--;
+
+        if(currPage>totalPage) currPage = totalPage;
+        if(currPage<this.state.initialPage) currPage = this.state.initialPage;
+
+        this.setState({
+            'currPage': currPage
+        });
+
+        if(currPage===1) sliceIndexes.push(0,perPage);
+        else if(currPage===totalPage) sliceIndexes.push((currPage-1)*perPage,this.props.totalItems)
+        else sliceIndexes.push((currPage - 1 )* perPage, currPage*perPage)
+
+        this.props.sliceArray(sliceIndexes);
+    }
+
+    render() {
+        let currPage = this.state.currPage;
+        let totalPage = this.state.totalPage;
+        let initialPage = this.state.initialPage;
+        let next = <button onClick={e=> {this.handlePageChange(1)}}>Next</button>;
+        let previous = <button onClick={e=> {this.handlePageChange(-1)}}>Previous</button>;
+        let buttons;
+
+        if(totalPage===1) {
+            buttons = '';
+        }
+        else if(currPage>=totalPage) {
+            buttons = previous;
+        }
+        else if(currPage<=initialPage) {
+            buttons = next;
+        }
+        else {
+            buttons = (
+                <div>
+                    {next}<span>{currPage}</span>{previous}
+                </div>
+            )
+        }
+        return (
+            <div className="paginator">
+                {buttons}
+            </div>
+        )
+    }
+}
